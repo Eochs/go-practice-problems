747d09a18c197544a3c35e8d0c3ef9b92ae35640
Spencer Ochs <slochastic@gmail.com>
2017-08-30 14:43:20 -0700

    finished solution that runs in O(n), space O(n)

--- longest-substring-without-repeating-characters/main.go
+++ longest-substring-without-repeating-characters/main.go
@@ -1,7 +1,26 @@
 package main
 
+func max(x, y int) int {
+	if x < y {
+		return y
+	}
+	return x
+}
+
+//LengthOfLongestSubstring ...
 func LengthOfLongestSubstring(s string) int {
-	return 3
+	n := len(s)
+	ans := 0
+	hashmap := map[byte]int{}
+	i := 0
+	for j := 0; j < n; j++ {
+		if v, ok := hashmap[s[j]]; ok { // s[j] is a duplicate value
+			i = max(v, i)
+		}
+		ans = max(ans, j-i+1)
+		hashmap[s[j]] = j + 1
+	}
+	return ans
 }
 
 func main() {
